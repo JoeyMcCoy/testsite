24710dd8ccd7bdcc92c04fcb7c4a99b0b68d609e
Joey McCoy <joey_mccoy@live.com>
2016-03-25 01:21:59 -0400

    page

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-angular.module('portfolio',[]).
+angular.module('portfolio', []).
 	config(function($routeProvider){
 		$routeProvider.
 			when('/1',{template:'partial.html'}).
-			otherwise({redirectTo:'/',template:'index.html'});
+			otherwise({redirectTo:'/',template:'testingpartial.html'});
 	});
 function MainCtrl($scope){
 	
