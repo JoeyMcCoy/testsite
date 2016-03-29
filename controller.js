6d6c06a51074e19a5a539df88d0cede9a5922cc5
Joey McCoy <joey_mccoy@live.com>
2016-03-29 12:42:45 -0400

    added projects and cleaned up structure

--- controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var app = angular.module('portfolio', []);
-portfolio.controller('PartialController',function($rootScope, $scope, $location, $routeParams){
-	 $scope.showPartial = function(pathurl){
-
-        console.log(pathurl)
-        $location.path(pathurl)
-    }
-});
